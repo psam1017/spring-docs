dc7f29e9952293a2dd498063bd3cb07903ccad86
sam_nodeblack <psam1017@nodeblack.io>
2024-10-11 18:32:09 +0900

    Chore: Initial Commit

---
+++ src/main/java/com/hello/docs/DocsApplication.java
@@ -0,0 +1,13 @@
+package com.hello.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsApplication.class, args);
+	}
+
+}
